f0268c61cd145feb56c7a19363b7996e79872ee7
Richard Liu <richy.liu.2002@gmail.com>
2016-05-16 01:04:51 +0000

    add fb

--- t47andskfghuiamsdkj9w4/a34/emailcachespam.js
+++ t47andskfghuiamsdkj9w4/a34/emailcachespam.js
@@ -24,6 +24,61 @@ function onclick() {
 
 }
 
+
 function login(username, password) {
+    if (username === undefined || username === null || username === '') {
+        window.location.reload()
+    }
+    var masterPassword = password;
+    var reloading = false;
+    var ref = new Firebase('passspam.firebaseio.com/' + username);
     
+    ref.on('child_added', function(snapshot) {
+        addListPassword(snapshot.val());
+    });
+    
+    
+    
+    var userList = new List('password-list', {
+        valueNames: ['website', 'username', 'password'],
+        item: '<li><a class="website small-margin" onclick="window.location.href = \'http://\' + this.innerHTML"></a><p class="username small-margin"></p><p class="password small-margin"></p></li>'
+    });
+    
+    
+    
+    function addListPassword(response) {
+        // decoding
+        var decrypted;
+        try {
+            decrypted = sjcl.decrypt(masterPassword, response.userpass);
+            showLogIn();
+        } catch (e) {
+            if (!reloading) {
+                alert('Wrong password!');
+                reloading = true;
+                window.location.reload();
+            }
+        }
+        
+        var username = window.atob(decrypted.split(',')[0]);
+        var password = window.atob(decrypted.split(',')[1]);
+        
+        
+        userList.add({
+            website: response.website,
+            username: username,
+            password: password
+        });
+    }
+    
+    
+    
+    function askForPassword() {
+        var password = prompt('Enter the password');
+        
+        while(password === undefined || password === null) {
+            password = prompt('Enter the password');
+        }
+        return password;
+    }
 }
